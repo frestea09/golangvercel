054415b9a9149b240368f245a96e96ad058cea6d
frestea09 <ilmanfrasetya@gmail.com>
2024-10-25 20:42:34 +0700

    good

--- index.go
+++ index.go
@@ -6,5 +6,9 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+
+    w.Header().Set("Content-Type", "text/html") // Set the correct content type
+
     fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+
 }
